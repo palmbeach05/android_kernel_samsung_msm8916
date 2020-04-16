c902b24f2efa062ca62ddc354a6d643fdcac51d8
palmbeach05 <davmcleod87@gmail.com>
2020-04-16 12:04:35 -0400

    Bringup to 3.10.108

--- net/ipv4/ping.c
+++ net/ipv4/ping.c
@@ -1126,9 +1126,9 @@ static void ping_format_sock(struct sock *sp, struct seq_file *f,
 
 static int ping_seq_show(struct seq_file *seq, void *v)
 {
-	seq_setwidth(seq, 127);
 	if (v == SEQ_START_TOKEN)
-		seq_puts(seq, "  sl  local_address rem_address   st tx_queue "
+		seq_printf(seq, "%-127s\n",
+			   "  sl  local_address rem_address   st tx_queue "
 			   "rx_queue tr tm->when retrnsmt   uid  timeout "
 			   "inode ref pointer drops");
 	else {
